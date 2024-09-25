feb3828b39d9dec3807e86cd24f562cbd8e53e8e
VA Quad <ethelinyama.va@gmail.com>
2024-09-26 00:02:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("HubSpot script loaded!");
+// Simple scroll to section functionality
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
